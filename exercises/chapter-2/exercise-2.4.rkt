b5215d484be582e5bad01700bc5a119c96f5cc62
Munir Zaman <munir.zaman2k22@outlook.com>
2024-01-21 22:23:13 +0600

    2.4 2.5

---
+++ exercises/chapter-2/exercise-2.4.rkt
@@ -0,0 +1,10 @@
+#lang sicp
+
+(define (mycons x y)
+  (lambda (m) (m x y)))
+
+(define (mycar z)
+  (z (lambda (p q) p)))
+
+(define (mycdr z)
+  (z (lambda (p q) q)))
---
+++ exercises/chapter-2/exercise-2.5.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+
+(define (mycons x y)
+    (* (expt 2 x) (expt 3 y)))
+
+; TODO
+(define (mycar z)
+    (1))
